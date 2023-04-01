81ccb9276774f97b1186e822053ccac842c0dab8
Wangshuanghu <103929424+Wangshuanghu@users.noreply.github.com>
2023-04-01 16:04:03 +0800

    Add files via upload

--- 制谱器.cpp
+++ 制谱器.cpp
@@ -1,32 +1,34 @@
-#include<iostream>
-#include<algorithm>
-#include<fstream>
-#include<cstring>
+#include <iostream>
+#include <algorithm>
+#include <fstream>
+#include <cstring>
 using namespace std;
 int main(){
-	ofstream out("Beatmap.cbm");
+	ofstream out ( "Beatmap.cbm" ) ;
 	int c;
 	string x; 
-	if(! out.is_open()){
-		cout << "Error opening file"; 
+	if( ! out.is_open() ){
+		cout << "Error opening file"; //读取错误 自动退出 
 		out.close();
 		return 0; 
 	}
 	int n;
-	cout<<"请输入note数量"<<":";
-	cin>>n;
-	for(int i=0;i<n;i++){
-		cout<<"请输入每个note的类型，注意换行"<<endl<<":"; 
-		cin>>x;
-		out<<x<<endl;
+	cout << "请输入note数量" << ":" ;
+	cin >> n ;
+	out << n << endl ;
+	for ( int i = 0 ; i < n ; i + +){
+		cout << "请输入每个note的类型，注意换行" << endl << ":" ; 
+		cin >> x ;
+		out << x << endl ;
 	}
-	for(int i=0;i<n;i++){
-		cout<<"请输入note出现与结束时间，注意请先出现后结束，两note间使用enter换行输入"<<endl<<":"; 
-		cin>>c;
-		out<<c<<" ";
-		cin>>c;
-		out<<c<<endl;
+	for ( int i = 0 ; i < n ; i++ ){
+		cout << "请输入note出现与结束时间，注意请先出现后结束，两note间使用enter换行输入" << endl << ":" ; 
+		cin >> c ;
+		out << c<< " " ;
+		cin >> c ;
+		out << c << endl ;
 	}
-	cout<<"输入完毕，请前往存放此文件的文件夹查收,文件名为beatmap.cpm"<<endl;
-	system("pause");
+	cout << "输入完毕，请前往存放此文件的文件夹查收,文件名为beatmap.cpm" << endl ;
+	system ( "pause" ) ;
+	return 0 ;
 }
